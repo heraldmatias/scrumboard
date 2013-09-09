55b53ddd69b974fac4d58f8c82dd93852c16d53a
heraldmatias <heraldmatias.oz@gmail.com>
2013-09-09 12:51:56 -0500

    locale

--- src/board/views.py
+++ src/board/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from core.decorators import render_template
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/admin/')
 @render_template
 def app(request):
     return "board/app.html"
 
+@login_required(login_url='/admin/')
 @render_template
 def home(request):
     return "index.html"
